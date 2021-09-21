6114a1956a5d473f0b91c118aaddd3ed34e6cb1b
Rahmat A Trikada <ralrafi12@gmail.com>
2021-09-21 13:38:55 +0700

    Scripting Enemy

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public SlingShooter SlingShooter;
     public List<Bird> Birds;
+    public List<Enemy> Enemies;
+
+    private bool _isGameEnded = false;
 
     private void Start()
     {
@@ -13,17 +15,44 @@ public class GameController : MonoBehaviour
         {
             Birds[i].OnBirdDestroyed += ChangeBird;
         }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
         
         SlingShooter.InitiateBird(Birds[0]);
     }
 
     public void ChangeBird()
     {
+        if (_isGameEnded)
+        {
+            return;
+        }
+        
         Birds.RemoveAt(0);
 
         if (Birds.Count > 0)
         {
             SlingShooter.InitiateBird(Birds[0]);
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
         }
     }
 }
